3c261d03304e865228108bc2c9c9a315ee426bda
NestorGH <nestornica10@gmail.com>
2025-07-12 17:48:07 -0600

    added auth and user resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { EmployeeModule } from './employee/employee.module';
 import { OwnerModule } from './owner/owner.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule, EmployeeModule, OwnerModule],
+  imports: [DatabaseModule, EmployeeModule, OwnerModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
