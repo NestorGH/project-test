6f786ed49ea5e61c5d1f2d2a96f8d5d8f35214f4
NestorGH <nestornica10@gmail.com>
2025-07-12 20:01:33 -0600

    added class validator

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,12 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 export class CreateUserDto {
     @ApiProperty()
+    @IsString()
     name: string;
 
     @ApiProperty()
     password: string;
 
     @ApiProperty()
+    @IsString()
     role: string;
 }
